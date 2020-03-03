f3351ec0236822d85a8870202ebec93c166636ad
Steve Locke <tap3stry@gmail.com>
2020-03-03 16:53:45 -0500

    Adding new class ExtremeAddition with a constructor

---
+++ src/main/java/org/apache/commons/lang3/math/ExtremeAddition.java
@@ -0,0 +1,12 @@
+package org.apache.commons.lang3.math;
+
+public class ExtremeAddition {
+
+	private int x;
+	private int y;
+	
+	public ExtremeAddition(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
