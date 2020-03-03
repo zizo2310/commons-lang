61af854192e540f00ce351883cf597cad2a94abd
Steve Locke <tap3stry@gmail.com>
2020-03-03 16:58:45 -0500

    Adding new class ExtremeDivision with a constructor

---
+++ src/main/java/org/apache/commons/lang3/math/ExtremeDivision.java
@@ -0,0 +1,12 @@
+package org.apache.commons.lang3.math;
+
+public class ExtremeDivision {
+
+	private int x;
+	private int y;
+	
+	public ExtremeDivision(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
