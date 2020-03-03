891d3e462d2b8d962f60a25a8f9ed7fb0f2f1a02
Steve Locke <tap3stry@gmail.com>
2020-03-03 16:56:37 -0500

    Adding new class ExtremeMultiplication with a constructor

---
+++ src/main/java/org/apache/commons/lang3/math/ExtremeMultiplication.java
@@ -0,0 +1,12 @@
+package org.apache.commons.lang3.math;
+
+public class ExtremeMultiplication {
+
+	private int x;
+	private int y;
+	
+	public ExtremeMultiplication(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
