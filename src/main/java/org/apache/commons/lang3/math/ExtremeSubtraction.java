54d9fb3101c5c7cc63434802cdacf786652fa3e0
Steve Locke <tap3stry@gmail.com>
2020-03-03 16:57:26 -0500

    Adding new class ExtremeSubtraction with a constructor

---
+++ src/main/java/org/apache/commons/lang3/math/ExtremeSubtraction.java
@@ -0,0 +1,12 @@
+package org.apache.commons.lang3.math;
+
+public class ExtremeSubtraction {
+
+	private int x;
+	private int y;
+	
+	public ExtremeSubtraction(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
